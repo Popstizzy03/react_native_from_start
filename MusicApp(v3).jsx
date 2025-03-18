168ac7bef62938d51791c51f4217ed6c833fcaff
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-03-18 18:51:30 +0200

    Update MusicApp(v3).jsx
    Constants

--- MusicApp(v3).jsx
+++ MusicApp(v3).jsx
@@ -18,3 +18,11 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import { Audio } from 'expo-av';
+
+// Constants
+const { width, height } = Dimensions.get('window');
+const CARD_MARGIN = 10;
+const GRID_SPACING = 16;
+const TRACK_CARD_WIDTH = (width - (GRID_SPACING * 3)) / 2;
+const MINI_PLAYER_HEIGHT = 80;
+const FULL_PLAYER_HEIGHT = height * 0.9;
