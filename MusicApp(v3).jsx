3a9ba127db3bfcc7a3dd5f21b903f39cb65bdeb4
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-03-18 18:50:33 +0200

    Update MusicApp(v3).jsx
    Imports

--- MusicApp(v3).jsx
+++ MusicApp(v3).jsx
@@ -1,1 +1,20 @@
 // Improved Ui Ux, basic expandable music conrol, music list 
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+  SafeAreaView,
+  StatusBar,
+  Animated,
+  PanResponder,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import { Audio } from 'expo-av';
