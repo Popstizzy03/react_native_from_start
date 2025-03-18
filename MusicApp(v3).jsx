3d1f7ca903d8f0e519b8cfcb81cf2d4264b86b14
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-03-18 18:55:34 +0200

    Update MusicApp(v3).jsx

--- MusicApp(v3).jsx
+++ MusicApp(v3).jsx
@@ -54,3 +54,75 @@ const SpotifyClone = () => {
   const soundRef = useRef(null);
   const playerAnimation = useRef(new Animated.Value(0)).current;
   const scrollViewRef = useRef(null);
+
+   // Data
+  const categories = [
+    'All',
+    'Pop',
+    'Hip-Hop',
+    'Rock',
+    'Electronic',
+    'Jazz',
+    'R&B',
+    'New Releases',
+  ];
+
+  // Replace the audio URLs with these real samples:
+  const playlist = [
+    {
+      id: '1',
+      title: 'Lucid Dreams',
+      artist: 'Juice WRLD',
+      image: 'https://picsum.photos/310',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Kai_Engel/Satin/Kai_Engel_-_07_-_Downfall.mp3',
+    },
+    {
+      id: '2',
+      title: 'Black Panther',
+      artist: 'Crystal Castles',
+      image: 'https://picsum.photos/299',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3',
+    },
+    {
+      id: '3',
+      title: 'Pausewidth',
+      artist: 'Aphex Twin',
+      image: 'https://picsum.photos/115',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3',
+    },
+    {
+      id: '4',
+      title: "Breakup with your girlfriend, I'm bored",
+      artist: 'Ariana Grande',
+      image: 'https://picsum.photos/208',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Audiobinger/The_Garden/Audiobinger_-_05_-_Garden_Instrumental.mp3',
+    },
+    {
+      id: '5',
+      title: 'Sucker',
+      artist: 'Jonas Brothers',
+      image: 'https://picsum.photos/399',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3',
+    },
+    {
+      id: '6',
+      title: 'Señorita',
+      artist: 'Shawn Mendes, Camila Cabello',
+      image: 'https://picsum.photos/303',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3',
+    },
+    {
+      id: '7',
+      title: 'Old Town Road',
+      artist: 'Lil Nas X, Billy Ray Cyrus',
+      image: 'https://picsum.photos/307',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Kai_Engel/Satin/Kai_Engel_-_04_-_Sentinel.mp3',
+    },
+    {
+      id: '8',
+      title: 'Bad Guy',
+      artist: 'Billie Eilish',
+      image: 'https://picsum.photos/308',
+      audio: 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3',
+    },
+  ];
